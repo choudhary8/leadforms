0a96de9f2384a29c6e401643b09466bb6c533065
Manoj Kumar <manojbhichchhar8@gmail.com>
2025-07-18 02:57:02 +0530

    style fix

--- src/components/InfoTab.tsx
+++ src/components/InfoTab.tsx
@@ -4,7 +4,7 @@ export const InfoTab=()=>{
             <div className="flex">
                 <div className="maroon font-bold text-2xl pb-1 border-b-1 mb-2">NRI Referral Form</div>
             </div>
-            <div className="bg-[#d75b15] font-semibold text-2xl text-white p-6 mb-4">Share your banking experience with your NRI family and friends</div>
+            <div className="bg-[#d75b15] font-semibold text-2xl text-white p-4 mb-4">Share your banking experience with your NRI family and friends</div>
 
             <div className="actionInfo my-2 ">
                 <div className="flex items-center w-2/3">
@@ -12,11 +12,11 @@ export const InfoTab=()=>{
                     <div>
                         <img src="https://buy.icicibank.com/ucj/media/iconPhone.a8ee7cc2.png" className="img-fluid ml-2" alt="icon1" width="35px" height="35px"></img>
                     </div>
-                    <div className="p-2 px-4">
+                    <div className="p-2 px-8">
                         <div className="text-[#d75b15] font-semibold">
                             Refer your NRI family and friends
                         </div>
-                        <div>Fill the form and get your family and friends on-call assistance for NRI account opening.</div>
+                        <div className="text-sm">Fill the form and get your family and friends on-call assistance for NRI account opening.</div>
                     </div>
                 </div>
                 <a href="https://www.icicibank.com/nri-banking/document_finder" className="flex items-center justify-end text-sm underline font-semibold">
@@ -31,11 +31,11 @@ export const InfoTab=()=>{
                 <div>
                     <img src="https://buy.icicibank.com//ucj/media/iconAssistance.d4e4674b.png" className="img-fluid ml-3" alt="icon2" width="35px" height="35px"></img>
                 </div>
-                <div className="p-2 px-4">
+                <div className="p-2 px-6">
                     <div className="text-[#d75b15] font-semibold">
                         Live Assistance from Team ICICI Bank!
                     </div>
-                    <div className="w-2/3">ICICI Bank will call and assist in filling the Account Opening form and KYC documentation.</div>
+                    <div className="w-2/3 text-sm">ICICI Bank will call and assist in filling the Account Opening form and KYC documentation.</div>
                 </div>
             </div>
 
@@ -45,11 +45,11 @@ export const InfoTab=()=>{
                     <div>
                         <img src="https://buy.icicibank.com/ucj/media/iconAccount.851bfc35.png" className="img-fluid ml-3" alt="icon3" width="35px" height="35px"></img>
                     </div>
-                    <div className="p-2 px-4">
+                    <div className="p-2 px-6">
                         <div className="text-[#d75b15] font-semibold">
                             NRI Account Opened
                         </div>
-                        <div>Account is opened after successful scrutiny of KYC documents.</div>
+                        <div className="text-sm">Account is opened after successful scrutiny of KYC documents.</div>
                     </div>
                 </div>
                 <p className="flex items-center justify-end text-sm underline font-semibold">
