0d40d91b16a28b35b21bb54a36801731868df3fc
choudhary8 <62460539+choudhary8@users.noreply.github.com>
2025-07-22 23:02:27 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,8 @@ export const Navbar=()=>{
     return (
         <div className="fixed w-screen bg-white top-0 z-1 sm:static">
             <div className="flex justify-between mx-3 items-center ">
-                <img src="https://buy.icicibank.com/ucj/media/icici3.1ed6e4c0.webp" alt="ICICI Bank" className="mb-1"></img>
+{/*                 <img src="https://buy.icicibank.com/ucj/media/icici3.1ed6e4c0.webp" alt="ICICI Bank" className="mb-1"></img> */}
+                <div className="font-extrabold text-3xl text-blue-950">My Bank</div>
                 <a href="https://icicibank.com">
                     <img loading="lazy" src="https://buy.icicibank.com/ucj/media/home-orange.85fa1234.webp" alt="ICICI Bank" className="p-2.5"></img>
                 </a>
