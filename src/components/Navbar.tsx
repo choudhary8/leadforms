65661353eff9ada12a6027587ba119ca4181ca80
choudhary8 <62460539+choudhary8@users.noreply.github.com>
2025-07-31 21:43:46 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import { IoHomeOutline } from "react-icons/io5"
 export const Navbar=()=>{
     return (
         <div className="fixed w-screen bg-white top-0 z-1 sm:static">
             <div className="flex justify-between mx-3 items-center ">
 {/*                 <img src="https://buy.icicibank.com/ucj/media/icici3.1ed6e4c0.webp" alt="ICICI Bank" className="mb-1"></img> */}
                 <div className="font-extrabold text-3xl text-blue-950">My Bank</div>
-                <a href="https://icicibank.com">
-                    <img loading="lazy" src="https://buy.icicibank.com/ucj/media/home-orange.85fa1234.webp" alt="ICICI Bank" className="p-2.5"></img>
+                <a href="https://icicibank.com" className="bg-[#e9822e] p-2 m-2 rounded-lg hover:bg-[#bf5702] shadow-xl">
+                    <IoHomeOutline className="font-bold text-3xl text-black"/>
+{/*                     <img loading="lazy" src="https://buy.icicibank.com/ucj/media/home-orange.85fa1234.webp" alt="ICICI Bank" className="p-2.5"></img> */}
                 </a>
             </div>
             <div className="bg-[#f27b1a] h-12"></div>
